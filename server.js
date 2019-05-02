5c13fa70bbbba16e02ce3c61dbf925750ff7e2a9
jasonasav <jasonasav@gmail.com>
2019-05-01 17:32:10 -0700

    deploying proxy to ec2

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+
+
 const port = process.env.port || 3050;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, './public')));
+
+// app.use('/api', proxy({ target: 'http://localhost:3000/', changeOrigin: true }))
+// app.listen(3000)
+
+app.use('/:urlId', express.static( path.join(__dirname, './public')));
 // app.use(express.static(path.join(__dirname + './node_modules')));
 
 app.listen(port, console.log(`proxy server is now listening on port ${port}`));
